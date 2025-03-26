bd1ba70da228f78f88069d6ef131fb7269e24b88
Mukai.T <127798683+okayus@users.noreply.github.com>
2025-03-27 00:45:56 +0900

    chore: add fields.d.ts template for type generation

---
+++ src/types/fields.d.ts
@@ -0,0 +1,26 @@
+/**
+ * kintoneアプリのフィールド型定義
+ * 
+ * このファイルは自動生成されます。手動で編集しないでください。
+ * 更新する場合は以下のコマンドを実行してください:
+ * 
+ * npm run generate-types
+ * 
+ * または:
+ * 
+ * npx kintone-dts-gen --base-url https://your-domain.cybozu.com \
+ *                      -u username \
+ *                      -p password \
+ *                      --app-id 123 \
+ *                      -o src/types/fields.d.ts
+ */
+
+declare namespace kintone.types {
+  interface Fields {
+    // @kintone/dts-gen 実行時に自動生成される型定義
+  }
+
+  interface SavedFields extends Fields {
+    // @kintone/dts-gen 実行時に自動生成される型定義
+  }
+}
